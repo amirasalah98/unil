685d7c5e8e0318cf4cef25c5caf51e7f0b34feb5
amirasalah98 <78090962+amirasalah98@users.noreply.github.com>
2023-05-11 14:54:03 +0300

    commit page2

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 // function nextAndPrev(){
 //     prev1.classList.add("")
 // }
+$(".start-quiz-btn").click(function(){
+    $(".section-home").hide();
+    $(".when-you-wish-all").show();
+  });
+  $(".prev1").click(function(){
+    $(".when-you-wish-all").hide();
+    $(".section-home").show();
+  });
